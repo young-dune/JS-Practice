e8bda26d2d8adade438bbdf8f86b17800ea5e4a9
SonYoungJun <dudwnsths1@naver.com>
2024-10-29 20:36:20 +0900

    10/29 commit

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden"; //중요한 정보를 담은게 아니라서 대문자로 작성함.
-
 function onLoginSubmit(info){ 
     info.preventDefault(); 
-    loginForm.classList.add(HIDDEN_CLASSNAME);
+    loginForm.classList.add("hidden");
     const username = loginInput.value;
-    localStorage.setItem("user", username);
+    localStorage.setItem("username", username);
     greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.classList.remove("hidden");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem("username");
+
+if(savedUsername === null){
+
+}else{
+
 }
 
 
