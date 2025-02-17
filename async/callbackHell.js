0ec10b974139a2c413f7f64abef4de39df79844a
SonYoungJun <dudwnsths1@naver.com>
2025-02-17 20:18:20 +0900

    callback hell + link

--- async/callbackHell.js
+++ async/callbackHell.js
@@ -7,3 +7,5 @@ function add(x, callback) {
 add(2, function (result)){
     add(result,function(result))
 }
+
+/* https://velog.io/@minidoo/%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8-%EC%BD%9C%EB%B0%B1-%ED%95%A8%EC%88%98Callback-Function */
